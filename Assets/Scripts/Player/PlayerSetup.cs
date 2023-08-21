0721e94aff85152b4d437574d5963d4ce1a5c5c0
vavarm <v.racaud-minuzzi@orange.fr>
2023-08-21 14:33:57 +0200

    feat: make the server able to spawn dummies

--- Assets/Scripts/Player/PlayerSetup.cs
+++ Assets/Scripts/Player/PlayerSetup.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-using HTNWIC.Camera;
+using HTNWIC.PlayerCamera;
 
 public class PlayerSetup : NetworkBehaviour
 {
