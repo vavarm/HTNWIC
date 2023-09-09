e85620ce1014809665a3b4b15c698ea8552233df
Valentin Racaud--Minuzzi <66647999+vavarm@users.noreply.github.com>
2023-09-09 21:40:33 +0200

    feat: add icon to sword

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace HTNWIC.Items
 {
     [CreateAssetMenu(fileName = "NewItem", menuName = "HTNWIC/Item")]
     public class Item : ScriptableObject
     {
-        public Image icon;
+        public Sprite icon;
     }
 }
